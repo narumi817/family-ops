fcaffc3a283ea430d260fc48778b9a1736210525
narumi817 <s2cartoon817@gmail.com>
2025-12-30 23:48:53 +0900

    諸々設定

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -5,12 +5,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # 開発環境では localhost からのアクセスを許可
+    # 本番環境では環境変数 ALLOWED_ORIGINS で指定（例: export ALLOWED_ORIGINS="https://example.com,https://www.example.com"）
+    origins Rails.env.development? ? /http:\/\/localhost(:\d+)?/ : ENV.fetch("ALLOWED_ORIGINS", "").split(",")
+
+    # 本番環境で直接オリジンを指定する場合の例（上記の行をコメントアウトして、こちらを有効化）:
+    # origins "https://example.com", "https://www.example.com"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
