51a59cb53a795fc8b72960ca91b3364d205f35bb
narumi817 <s2cartoon817@gmail.com>
2026-02-23 23:06:57 +0900

    Clarityを導入

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      script: [
+        {
+          // Microsoft Clarity
+          innerHTML:
+            '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "vlwk2jlppw");',
+        } as any,
+      ] as any,
+    },
+  },
   runtimeConfig: {
     public: {
       baseURL: process.env.BASE_URL || 'http://localhost:3000',
